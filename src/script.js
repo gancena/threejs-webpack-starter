bb302ea6ceb6c4b3fe4360f0ee776b55946446cd
Eli Gancena <eli@playgroundinc.com>
2021-04-23 14:17:46 -0300

    healthspan customizations

--- src/script.js
+++ src/script.js
@@ -12,25 +12,64 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Texture Loader
+const loader = new THREE.TextureLoader();
+const cross = loader.load('./cross.png');
+
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const sphereGeometry = new THREE.SphereBufferGeometry( .7, 32, 32);
+const starFieldGeometry = new THREE.BufferGeometry;
+const particlesCnt = 5000;
+
+const posArray = new Float32Array(particlesCnt * 3);
+
+for ( let i = 0; i < particlesCnt * 3; i++ ) {
+    posArray[i] = (Math.random() - 0.5) * (Math.random() * 5);
+}
+
+starFieldGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// particlesSphere
+// const sphereMaterial = new THREE.PointsMaterial({
+//     size: 0.005
+// })
+
+// solidSphere
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    roughness: 0.2,
+    transparent: false,
+    opacity: 1,
+    wireframe: true // disable when done
+})
+
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.005,
+    map: cross,
+    transparent: true,
+    // color: 'white',
+    // blending: THREE.AdditiveBlending
+})
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+const starField = new THREE.Points(starFieldGeometry, particlesMaterial)
+scene.add(sphere, starField)
 
 // Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.set(2,3,4)
 scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xffffff, 0.5)
+pointLight2.position.set(-2,-3,-4)
+scene.add(pointLight2)
+
+const pointLight3 = new THREE.PointLight(0xffff00, 0.5)
+pointLight3.position.set(6,6,6)
+scene.add(pointLight3)
 
 /**
  * Sizes
@@ -73,25 +112,50 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// renderer.setClearColor(new THREE.Color('#21282a'), 1)
 
+// Mouse
+document.addEventListener('mousemove', animateParticles);
+
+let mouseX = 0;
+let mouseY = 0;
+
+function animateParticles(e) {
+    mouseY = e.clientY * 0.1;
+    mouseX = e.clientX * 0.1;
+}
 /**
  * Animate
  */
 
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    // sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.z = .05 * elapsedTime
 
+    // animate before mousemove
+    starField.rotation.y = elapsedTime * -0.01;
+    starField.rotation.x = elapsedTime * -0.01;
+    
+    // animate on mousemove
+    if (mouseX > 0) {
+        starField.rotation.x = -mouseY * (elapsedTime * 0.00004);
+        starField.rotation.y = -mouseX * (elapsedTime * 0.00004);
+
+        sphere.rotation.x = mouseY * (1.1 * 0.0003);
+        sphere.rotation.y = mouseX * (1.1 * 0.0003);
+    }
+    
     // Update Orbital Controls
     // controls.update()
 
